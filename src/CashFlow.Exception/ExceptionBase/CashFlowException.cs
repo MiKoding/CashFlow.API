fd2bf14b13f7d48b937feaac80dc21e121ef5317
MiKoding <mikaioharison038@gmail.com>
2024-08-16 16:27:38 -0300

    Banco de dados e Injeção de dependência - Modulo 5: Tratando exception quando ID não existe

--- src/CashFlow.Exception/ExceptionBase/CashFlowException.cs
+++ src/CashFlow.Exception/ExceptionBase/CashFlowException.cs
@@ -7,4 +7,8 @@ using System.Threading.Tasks;
 namespace CashFlow.Exception.ExceptionBase;
 public abstract class CashFlowException : SystemException
 {
+    protected CashFlowException(string message) : base(message) 
+    {
+        
+    }
 }
