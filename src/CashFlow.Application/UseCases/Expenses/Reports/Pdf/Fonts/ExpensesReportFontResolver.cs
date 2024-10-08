df4c4565e821800ba81ceb87cca9060612bdf12a
MiKoding <mikaioharison038@gmail.com>
2024-10-08 16:24:11 -0300

    Gerando relatórios em Excel e PDF - Modulo 2: Criando fonte resolver

---
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/Fonts/ExpensesReportFontResolver.cs
@@ -0,0 +1,23 @@
+﻿using MigraDoc.DocumentObjectModel;
+using PdfSharp.Fonts;
+
+namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
+public class ExpensesReportFontResolver : IFontResolver
+{
+    public byte[]? GetFont(string faceName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public FontResolverInfo? ResolveTypeface(string familyName, bool bold, bool italic)
+    {
+        var font = new Font
+        {
+            Name = "WorkSans",
+            Bold = true,
+            Italic = false,
+        };
+
+        return new FontResolverInfo("WorkSans");
+    }
+}
