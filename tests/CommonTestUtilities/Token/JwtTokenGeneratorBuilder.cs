a1929951de134f61998b2f73c58dc3288d2b1d45
MiKoding <mikaioharison038@gmail.com>
2025-05-10 18:38:09 -0300

    Implementa teste de registro de usuario

---
+++ tests/CommonTestUtilities/Token/JwtTokenGeneratorBuilder.cs
@@ -0,0 +1,16 @@
+﻿using CashFlow.Domain.Entities;
+using CashFlow.Domain.Security.Tokens;
+using Moq;
+
+namespace CommonTestUtilities.Token;
+public class JwtTokenGeneratorBuilder
+{
+    public static IAccessTokenGenerator Build()
+    {
+        var mock = new Mock<IAccessTokenGenerator>();
+
+        mock.Setup(accessTokenGenerator => accessTokenGenerator.Generate(It.IsAny<User>())).Returns("O12IU35G34HJ534HJV5JH34VJH324VJH534JKNFNIJRF");
+
+        return mock.Object; 
+    }
+}
--- tests/UseCases.Test/Users/Register/RegisterUserUseCaseTest.cs
+++ tests/UseCases.Test/Users/Register/RegisterUserUseCaseTest.cs
@@ -1,7 +1,9 @@
 ﻿using CashFlow.Application.UseCases.Users.Register;
+using CommonTestUtilities.Cryptography;
 using CommonTestUtilities.Mapper;
 using CommonTestUtilities.Repositories;
 using CommonTestUtilities.Requests;
+using CommonTestUtilities.Token;
 using FluentAssertions;
 
 namespace UseCases.Test.Users.Register;
@@ -25,7 +27,10 @@ public class RegisterUserUseCaseTest
         var mapper = MapperBuilder.Build();
         var uniteOfWork = UnitOfWorkBuilder.Build();
         var writeRepository = UserWriteOnlyRepositoryBuilder.Build();
+        var passwordEncripter = PasswordEncripterBuilder.Build();
+        var jwtToken = JwtTokenGeneratorBuilder.Build();
+        var readRepository = new UserReadOnlyRepositoryBuilder().Build();
 
-        return new RegisterUserUseCase(mapper, null, null, writeRepository, uniteOfWork);
+        return new RegisterUserUseCase(mapper, passwordEncripter, readRepository, writeRepository, uniteOfWork, jwtToken);
     }
 }
