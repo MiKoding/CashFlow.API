f7b31c5c21b96ef5b9107be442102c98c51e0798
MiKoding <mikaioharison038@gmail.com>
2025-05-23 13:49:41 -0300

    implementa teste de login e dados na memoria

--- tests/WebApi.IntegrationTest/CustomWebApplicationFactory.cs
+++ tests/WebApi.IntegrationTest/CustomWebApplicationFactory.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Infraestructure.DataAccess;
+using CommonTestUtilities.Entities;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
@@ -20,6 +21,19 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
                    config.UseInternalServiceProvider(provider);
                 }
                     );
+                var scope = services.BuildServiceProvider().CreateScope();
+                var dbContext = scope.ServiceProvider.GetRequiredService<CashFlowDbContext>();
+
+                StartDataBase(dbContext);
             });
+    }
+
+    private void StartDataBase(CashFlowDbContext dbContext)
+    {
+        var user = UserBuilder.Build();
+
+        dbContext.Users.Add(user);
+
+        dbContext.SaveChanges();
     }
 }
---
+++ tests/WebApi.IntegrationTest/Login/DoLogin/DoLoginTest.cs
@@ -0,0 +1,39 @@
+﻿using CashFlow.Communication.Requests;
+using FluentAssertions;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace WebApi.IntegrationTest.Login.DoLogin;
+public class DoLoginTest : IClassFixture<CustomWebApplicationFactory>
+{
+    private const string METHOD = "api/Login";
+
+    private readonly HttpClient _httpClient;
+
+    public DoLoginTest(CustomWebApplicationFactory factory)
+    {
+        _httpClient = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task Successs()
+    {
+        var request = new RequestLoginJson 
+        {
+            Email = 
+        };
+
+        var response = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var responseBody = await response.Content.ReadAsStreamAsync();
+
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+
+        responseData.RootElement.GetProperty("name").GetString().Should().Be(_name);
+        responseData.RootElement.GetProperty("token").GetString().Should().NotBeNullOrWhiteSpace();
+
+    }
+}
