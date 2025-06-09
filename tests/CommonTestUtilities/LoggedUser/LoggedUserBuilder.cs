bfb9e0a8b353127ea5318593aa4b834130dafe55
MiKoding <mikaioharison038@gmail.com>
2025-06-08 21:46:56 -0300

    implementa teste de registro de expense

---
+++ tests/CommonTestUtilities/LoggedUser/LoggedUserBuilder.cs
@@ -0,0 +1,16 @@
+﻿using CashFlow.Domain.Entities;
+using CashFlow.Domain.Services.ILoggedUser;
+using Moq;
+
+namespace CommonTestUtilities.LoggedUser;
+public class LoggedUserBuilder
+{
+    public static IloggedUser Build(User user)
+    {
+        var mock = new Mock<IloggedUser>();
+
+        mock.Setup(loggedUser => loggedUser.Get()).ReturnsAsync(user);
+
+        return mock.Object; 
+    }
+}
---
+++ tests/WebApi.IntegrationTest/Expenses/Register/RegisterExpenseUseCaseTest.cs
@@ -0,0 +1,53 @@
+﻿using CommonTestUtilities.Mapper;
+using CommonTestUtilities.Repositories;
+using CommonTestUtilities.LoggedUser;
+using CashFlow.Application.UseCases.Expenses.Register;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+using CashFlow.Exception.ExceptionBase;
+using CashFlow.Exception;
+
+namespace WebApi.IntegrationTest.Expenses.Register;
+public class RegisterExpenseUseCaseTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        var loggedUser = UserBuilder.Build();
+        var request = RequestsRegisterExpensesJsonBuilder.Build();
+        var useCase = CreateUseCase(loggedUser);
+
+        var result = await useCase.Execute(request);
+
+        result.Should().NotBeNull();
+        result.Title.Should().Be(request.Title);
+    }
+    [Fact]
+    public async Task Error_Title_Empty()
+    {
+        var loggedUser = UserBuilder.Build();
+
+        var request = RequestsRegisterExpensesJsonBuilder.Build();
+        request.Title = string.Empty;
+
+        var useCase = CreateUseCase(loggedUser);
+
+        var act = async() => await useCase.Execute(request);
+
+        var result = await act.Should().ThrowAsync<ErrorOnValidationException>();
+
+        result.Where(ex => ex.GetErrors().Count == 1 && ex.GetErrors().Contains(ResourceErrorMessages.TITLE_REQUIRED));
+
+    }
+    private RegisterExpensesUseCase CreateUseCase(CashFlow.Domain.Entities.User user)
+    {
+        var repository = ExpenseWriteOnlyRepository.Build();
+        var mapper = MapperBuilder.Build();
+        var unitOfWork = UnitOfWorkBuilder.Build();
+        var loggedUser = LoggedUserBuilder.Build(user);
+
+        return new RegisterExpensesUseCase(repository,unitOfWork,mapper,loggedUser);
+
+    }
+}
